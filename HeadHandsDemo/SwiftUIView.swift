e0482c828a1f5298b99377e841cef377f3171a0f
Jerry Wang <jerrywrh@qq.com>
2024-08-13 20:52:28 +0800

    feat: Hand Tracking demo done

---
+++ HeadHandsDemo/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  HeadHandsDemo
+//
+//  Created by Jerry Wang on 2024/8/8.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ HeadHandsDemo/🛠️Menu/JonintPositionsPanel.swift
@@ -0,0 +1,18 @@
+//
+//  JonintPositionsPanel.swift
+//  HeadHandsDemo
+//
+//  Created by Jerry Wang on 2024/8/9.
+//
+
+import SwiftUI
+
+struct JonintPositionsPanel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JonintPositionsPanel()
+}
